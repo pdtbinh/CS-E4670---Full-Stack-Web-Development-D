1aa1ba9fdc9a1cab036e9708e4ef6c42e94c0683
pdtieubinh <tinybottles2112@gmail.com>
2023-06-22 11:45:43 +0300

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 require('express-async-errors')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1 })
     response.status(200).json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
+    // Initial checks
     if (!request.body.likes) request.body.likes = 0
     if (!request.body.title || !request.body.url) return response.status(400).end()
-    const blog = new Blog(request.body)
-    const result = await blog.save()
-    response.status(201).json(result)
+    
+    // Create blog
+    const users = await User.find({})
+    const user = users[0]
+    const blog = new Blog({ ...request.body, user: user._id })
+    const savedBlog = await blog.save()
+
+    // Add blog to user.blogs
+    user.blogs = user.blogs.concat(savedBlog._id)
+    user.save()
+
+    // Respond
+    response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
--- controllers/user.js
+++ controllers/user.js
@@ -3,13 +3,20 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+        .find({})
+        .populate('blogs', { 
+            title: 1,
+            author: 1,
+            url: 1,
+            likes: 1
+        })
     response.status(200).json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
-    if (password.length < 4) 
+    if (password.length < 3) 
         return response.status(400).json(
             { error: 'passowrd must have at least 3 characters.' }
         )
