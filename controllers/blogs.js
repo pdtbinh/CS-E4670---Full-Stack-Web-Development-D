88e529da8071d61adc802aceec01d98e606d1e80
pdtieubinh <tinybottles2112@gmail.com>
2023-06-22 15:23:26 +0300

    4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -34,8 +34,18 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-    const { id } = request.params
-    const deleted = await Blog.findByIdAndRemove(id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    
+    const blog = await Blog.findById(request.params.id)
+    const user = await User.findById(decodedToken.id)
+    
+    if (blog.user._id.toString() !== user._id.toString()) {
+        return response.status(401).json({ error: 'unauthorized request' })
+    }
+    const deleted = await Blog.findByIdAndRemove(request.params.id)
     if (deleted)
         response.status(204).end()
     else
