8ae38a8448ba19f72814ac7ce5a531da7d5734ce
pdtieubinh <tinybottles2112@gmail.com>
2023-07-16 13:05:35 +0300

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,7 @@
-const Blog = ({blog}) => {
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -7,9 +10,24 @@ const Blog = ({blog}) => {
     marginBottom: 5
   }
 
-  return (<div style={blogStyle}>
-    {blog.title} {blog.author}
-  </div>)
+  const [hidden, setHidden] = useState(true)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {hidden ? blog.author : null}
+
+      {hidden && <button onClick={() => setHidden(false)}>View</button>}
+      
+      {hidden || (
+        <>
+          <button onClick={() => setHidden(true)}>Hide</button>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+        </>
+      )}
+    </div>
+  )
 }
 
 export default Blog
